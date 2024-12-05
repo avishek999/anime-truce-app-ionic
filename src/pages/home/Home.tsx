ce59c24d01e672e50ba9fafddc42a9d28b2b5d41
Avishek Prasad <avishekprasad0999@gmail.com>
2024-12-05 22:00:15 +0530

    change auth to more robust and secure

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,7 +5,6 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import ExploreContainer from "../../components/ExploreContainer";
 import "./Home.scss";
 import Wrapper from "../../shared/utils/wrapper/Wrapper";
 import TabBar from "../../components/TabBar";
