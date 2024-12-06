db238ed2251ddefd722ce82979599381f767fff5
Avishek Prasad <avishekprasad0999@gmail.com>
2024-12-06 12:57:46 +0530

    swipper added

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,26 +1,45 @@
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-} from "@ionic/react";
+
 import "./Home.scss";
 import Wrapper from "../../shared/utils/wrapper/Wrapper";
 import TabBar from "../../components/TabBar";
+import BannerSwiper from "../../components/home/BannerSwiper/BannerSwiper";
 
 
-import {FetchAnime} from '../../api/api'
-import { useEffect } from "react";
+const animeSlides = [
+  {
+    id: "1",
+    image: "https://cdn.noitatnemucod.net/thumbnail/300x400/100/9b25eb117ecc8cf228d361540c7cfd67.jpg",
+    title: "Naruto: Believe It!",
+  },
+  {
+    id: "2",
+    image: "https://cdn.noitatnemucod.net/thumbnail/300x400/100/9b25eb117ecc8cf228d361540c7cfd67.jpg",
+    title: "One Piece: The Pirate's Journey",
+  },
+  {
+    id: "3",
+    image: "https://cdn.noitatnemucod.net/thumbnail/300x400/100/9b25eb117ecc8cf228d361540c7cfd67.jpg",
+    title: "Attack on Titan: Humanity's Struggle",
+  },
+  {
+    id: "4",
+    image: "https://cdn.noitatnemucod.net/thumbnail/300x400/100/9b25eb117ecc8cf228d361540c7cfd67.jpg",
+    title: "Demon Slayer: Kimetsu no Yaiba",
+  },
+  {
+    id: "5",
+    image: "https://cdn.noitatnemucod.net/thumbnail/300x400/100/9b25eb117ecc8cf228d361540c7cfd67.jpg",
+    title: "My Hero Academia: Heroes Rising",
+  },
+  
+];
+
+
+
 const Home: React.FC = () => {
-
-
-
   return (
     <Wrapper>
-      <h1 className="text-center"> Home </h1>
-
-      {}
+      <BannerSwiper slides={animeSlides} />
 
       <TabBar />
     </Wrapper>
